e13e984336903e25e0f44fee5fef4a00fa2cab33
jenilbanavani <jbanavani@email.com>
2026-05-11 17:41:10 +0530

    Updated-11/5/26

--- kilo.c
+++ kilo.c
@@ -59,7 +59,24 @@ char editorReadKey(){
   while(( nread = read(STDIN_FILENO,&c,1)) != 1){
     if( nread == -1 && errno != EAGAIN) die("read");
   }
-return c;
+  if( c == '\x1b'){
+    char seq[3];
+
+    if(read(STDIN_FILENO,&seq[0],1) != 1) return '\x1b';
+    if(read(STDIN_FILENO,&seq[1],1) != 1) return '\x1b';
+
+    if( seq[0] == '['){
+      switch(seq[1]){
+        case 'A' : return 'w';
+        case 'B' : return 's';
+        case 'C' : return 'd';
+        case 'D' : return 'a';
+      }
+    }
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
 
 int getCursorPosition( int *rows, int *cols){
@@ -156,12 +173,33 @@ void editorRefreshScreen(){
   char buf[32];
   snprintf(buf, sizof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
   abAppend(&ab,buf, strlen(buf));
+
   abAppend(&ab,"\x1b[?25h",6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
 }
 /*** input ***/
+
+void editorMoveCursor( char key){
+  switch( key ){
+    case 'a' :
+    E.cx--;
+    break;
+
+    case 'd' :
+    E.cx++;
+    break;
+
+    case 'w' :
+    E.cy--;
+    break;
+
+    case 's' :
+    E.cy++;
+    break;
+  }
+}
 void editorProcessKeypress(){
   char c = editorReadKey();
 
@@ -170,6 +208,13 @@ void editorProcessKeypress(){
       write(STDOUT_FILENO,"\x1b[2j",4);
       write(STDOUT_FILENO,"\x1b[H",3);
       exit 0;
+      break;
+
+    case 'w':
+    case 's':
+    case 'a':
+    case 'd':
+      editorMoveCursor(c);
       break;
   }
 }
